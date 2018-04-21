19cf132de822b2e23e20d25974f281ca80177d8c
Troy Taylor <troytaylor.net@gmail.com>
2018-04-21 15:54:27 -0400

    singleton creational pattern

---
+++ DoFactory.GangOfFour/Proxy.Structural/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy.Structural
+{
+    class Program
+    {
+        static void Main()
+        {
+            // create proxy and request a service
+            Proxy proxy = new Proxy();
+            proxy.Request();
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Subject
+    {
+        public abstract void Request();
+    }
+
+    class RealSubject : Subject
+    {
+        public override void Request()
+        {
+            Console.WriteLine("Called RealSubject.Request()");
+        }
+    }
+
+    class Proxy : Subject
+    {
+        private RealSubject _realSubject;
+
+        public override void Request()
+        {
+            if (_realSubject == null)
+            {
+                _realSubject = new RealSubject();
+            }
+
+            _realSubject.Request();
+        }
+    }
+}
---
+++ DoFactory.GangOfFour/Singleton.NETOptimized/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Singleton.NETOptimized
+{
+    class Program
+    {
+        static void Main()
+        {
+            var b1 = LoadBalancer.GetLoadBalancer();
+            var b2 = LoadBalancer.GetLoadBalancer();
+            var b3 = LoadBalancer.GetLoadBalancer();
+            var b4 = LoadBalancer.GetLoadBalancer();
+
+            // Confirm these are the same instance
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+
+            var balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 15; i++)
+            {
+                string serverName = balancer.NextServer.Name;
+                Console.WriteLine("Dispatch request to: "
+                    + serverName);
+            }
+
+            // wait for user
+            Console.ReadKey();
+        }
+    }
+
+    sealed class LoadBalancer
+    {
+        private static readonly LoadBalancer _instance =
+            new LoadBalancer();
+
+        // type-safe generic list of servers
+        private List<Server> _servers;
+        private Random _random = new Random();
+
+        // constructor
+        private LoadBalancer()
+        {
+            _servers = new List<Server>
+            {
+                new NETOptimized.Server { Name = "ServerI", IP = "120.14.220.18" },
+                new NETOptimized.Server { Name = "ServerII", IP = "120.14.220.19" },
+                new NETOptimized.Server { Name = "ServerII", IP = "120.14.220.20" },
+                new NETOptimized.Server { Name = "ServerIV", IP = "120.14.220.21" },
+                new NETOptimized.Server { Name = "ServerV", IP = "120.14.220.22" }
+            };
+        }
+
+        public static LoadBalancer GetLoadBalancer()
+        {
+            return _instance;
+        }
+
+        public Server NextServer
+        {
+            get
+            {
+                int r = _random.Next(_servers.Count);
+                return _servers[r];
+            }
+        }
+    }
+
+    class Server
+    {
+        public string Name { get; set; }
+        public string IP { get; set; }
+    }
+}
