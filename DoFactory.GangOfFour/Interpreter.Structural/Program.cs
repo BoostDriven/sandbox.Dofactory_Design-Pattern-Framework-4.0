45ad39460e14c13d962936a8c7fa59c38e3c0349
tptaylor <troytaylor.net@gmail.com>
2018-06-06 15:28:10 -0400

    Interpreter pattern

---
+++ DoFactory.GangOfFour/Interpreter.Structural/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter.RealWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Context context = new Context();
+            ArrayList list = new ArrayList();
+
+            list.Add(new TerminalExpression());
+            list.Add(new NonSerializedAttribute());
+            list.Add(new TerminalExpression());
+            list.Add(new TerminalExpression());
+
+            foreach (AbstractExpression exp in list)
+            {
+                exp.Interpret(context);
+            }
+
+            // wait for user
+            Console.ReadKey();
+        }
+    }
+
+    class Context {
+    }
+
+    abstract class AbstractExpression
+    {
+        public abstract void Interpret(Context context);
+    }
+
+    class TerminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("Called Terminal.Interpret()");
+        }
+    }
+
+    class NonterminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("Called. Nonterminal.Interpret()");
+        }
+    }
+}
---
+++ DoFactory.GangOfFour/Visitor.Structural/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor.Structural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Setup structure
+            ObjectStructure o = new ObjectStructure();
+            o.Attach(new ConcreteElementA());
+            o.Attach(new ConcreteElementB());
+
+            // Create visitor objects
+            ConcreteVistor1 v1 = new ConcreteVistor1();
+            ConcreteVistor2 v2 = new ConcreteVistor2();
+
+            // Structure accepting visitors
+            o.Accept(v1);
+            o.Accept(v2);
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Visitor
+    {
+        public abstract void VisitConcreteElementA(ConcreteElementA concreteElementA);
+        public abstract void VisitConcreteElementB(ConcreteElementB concreteElementB);
+    }
+
+    class ConcreteVistor1 : Visitor
+    {
+        public override void VisitConcreteElementA(ConcreteElementA concreteElementA)
+        {
+            Console.WriteLine("{0} visited by {1}", concreteElementA.GetType().Name, this.GetType().Name);
+        }
+
+        public override void VisitConcreteElementB(ConcreteElementB concreteElementB)
+        {
+            Console.WriteLine("{0} visited by {1}", concreteElementB.GetType().Name, this.GetType().Name);
+        }
+    }
+
+    class ConcreteVistor2 : Visitor
+    {
+        public override void VisitConcreteElementA(ConcreteElementA concreteElementA)
+        {
+            Console.WriteLine("{0} visited by {1}", concreteElementA.GetType().Name, this.GetType().Name);
+        }
+
+        public override void VisitConcreteElementB(ConcreteElementB concreteElementB)
+        {
+            Console.WriteLine("{0} visited by {1}", concreteElementB.GetType().Name, this.GetType().Name);
+        }
+    }
+
+    abstract class Element
+    {
+        public abstract void Accept(Visitor visitor);
+    }
+
+    class ConcreteElementA : Element
+    {
+        public override void Accept(Visitor visitor)
+        {
+            visitor.VisitConcreteElementA(this);
+        }
+    }
+
+    class ConcreteElementB : Element
+    {
+        public override void Accept(Visitor visitor)
+        {
+            visitor.VisitConcreteElementB(this);
+        }
+    }
+
+    class ObjectStructure
+    {
+        private List<Element> _elements = new List<Element>();
+
+        public void Attach(Element element)
+        {
+            _elements.Add(element);
+        }
+
+        public void Detach(Element element)
+        {
+            _elements.Remove(element);
+        }
+
+        public void Accept(Visitor visitor)
+        {
+            foreach (Element element in _elements)
+            {
+                element.Accept(visitor);
+            }
+        }
+    }
+}
